b999d6cc4e6090633e8b8f8c28bf335b3f418ac0
Gopinath Kathirvel <104065528+Gopinathgopi13@users.noreply.github.com>
2023-11-24 06:23:18 +0530

    Delete Random Joke Generator directory

--- Random Joke Generator/app.js
+++
@@ -1,8 +0,0 @@
-// API_link = https://v2.jokeapi.dev/joke/Any
-
-let para =  document.getElementById("para");
-async function generate(){
-    let api = await fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single")
-    let data = await api.json();
-    para.innerHTML = data.joke
-}
